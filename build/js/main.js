9a46885b10e66579c83649f7db7d821fd43f2207
Héctor Palma Téllez <hector@palmatellez.cl>
2014-11-30 00:23:00 -0300

    + build.

---
+++ build/js/main.js
@@ -0,0 +1,1 @@
+$(function(){var a="line://msg/text/",b="whatsapp://send?text:",c=document.title,d=document.URL;$(".line-btn").attr("href",a+"Cáchate esto: "+c+d),$(".whatsapp-btn").attr("href",b+"Cáchate esto: "+c+d)});
