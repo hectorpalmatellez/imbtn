5d2110743f95dca9d2f256db98ca66d533252b35
Héctor Palma Téllez <hector@palmatellez.cl>
2014-11-30 00:49:54 -0300

    Último build.

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,1 @@
-$(function(){var a="line://msg/text/",b="whatsapp://send?text:",c=document.title,d=document.URL;$(".line-btn").attr("href",a+"Cáchate esto: "+c+d),$(".whatsapp-btn").attr("href",b+"Cáchate esto: "+c+d)});
+$(function(){var a="line://msg/text/",b="whatsapp://send?text=",c=document.title,d=document.URL,e="Cáchate esto:";$(".line-btn").attr("href",a+e+" "+c+" "+d),$(".whatsapp-btn").attr("href",b+e+" "+c+" "+d)});
